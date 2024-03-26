7835410b766364ae2c214296c7bd08ae7cba8a32
StefanMaring <smaring@ziggo.nl>
2024-03-26 10:01:35 +0100

    Added test project

---
+++ ShowcaseBackendTests/HTML_Sanitize_Test.cs
@@ -0,0 +1,27 @@
+﻿using Ganss.Xss;
+using Rest_API_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowcaseBackendTests {
+    [TestFixture]
+    public class HTML_Sanitize_Test {
+        [Test]
+        public void testHTMLSanitization() {
+            string htmlWithScriptTag = "<script>alert('This is a script tag');</script><iframe></iframe>";
+            string htmlWithAllowedTags = "<h3>This is a heading</h3><p>This is a paragraph</p>";
+
+            string sanitizedHtmlWithScriptTag = FormValidation.SanitizeHtml(htmlWithScriptTag);
+            string sanitizedHtmlWithAllowedTags = FormValidation.SanitizeHtml(htmlWithAllowedTags);
+
+            Assert.IsFalse(sanitizedHtmlWithScriptTag.Contains("<script>"));
+            Assert.IsFalse(sanitizedHtmlWithScriptTag.Contains("<iframe>"));
+
+            Assert.IsTrue(sanitizedHtmlWithAllowedTags.Contains("<h3>"));
+            Assert.IsTrue(sanitizedHtmlWithAllowedTags.Contains("<p>"));
+        }
+    }
+}
